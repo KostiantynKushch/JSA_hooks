2a6fea3e7cb2576fc2a919a0693fb011d32e540a
romannikiforov <escaro@ukr.net>
2019-03-17 06:08:48 +0200

    start 09_homework

---
+++ src/components/PrintScrollToBottom.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { lifecycle } from "recompose";
+
+const scroll = () => window.scrollTo(0, document.documentElement.scrollHeight);
+
+const PrintScrollToBottom = ({ children }) => children;
+
+const enhance = lifecycle({
+  componentDidMount() {
+    scroll();
+  },
+  getSnapshotBeforeUpdate() {
+    const { clientHeight, scrollTop, scrollHeight } = document.documentElement;
+    return clientHeight + scrollTop < scrollHeight;
+  },
+  componentDidUpdate(prevProps, prevState, isScrolledUp) {
+    if (!isScrolledUp) scroll();
+  },
+});
+
+export default enhance(PrintScrollToBottom);
