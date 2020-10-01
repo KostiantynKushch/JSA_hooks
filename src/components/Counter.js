d64f18fc66d1817757e171b47b96ef9b6ba5d896
kostiantynkushch <kostiantyn.kushch@gmail.com>
2020-10-02 00:43:21 +0300

    counter with reducere

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,46 @@
 import React, { useReducer } from "react";
 
-const initialValue = {
-  count: 0
-};
-const Counter = () => {
-  const handleClick = () => {};
+const reducer = (state, action) => {
+	const { count } = state;
 
-  return (
-    <>
-      <h1>Counter: 0 </h1>
-      <button onClick={handleClick} className="btn btn-primary mr-3">
-        Decrement
+	switch (action.type) {
+		case "increment":
+			return { ...state, count: count + 1 }
+		case "decrement":
+			return { ...state, count: count - 1 }
+		case "reset":
+			return { ...state, count: 0 }
+		default:
+			throw Error('no cases');
+	}
+}
+
+
+const initialValue = {
+	count: 0
+};
+
+const Counter = () => {
+	const [{ count }, dispatch] = useReducer(reducer, initialValue);
+
+	const handleClick = (action) => {
+		dispatch({ type: action });
+	};
+
+	return (
+		<>
+			<h1>Counter: {count} </h1>
+			<button onClick={() => handleClick('decrement')} className="btn btn-primary mr-3">
+				Decrement
       </button>
-      <button onClick={handleClick} className="btn btn-warning mr-3">
-        Reset
+			<button onClick={() => handleClick('reset')} className="btn btn-warning mr-3">
+				Reset
       </button>
-      <button onClick={handleClick} className="btn btn-success">
-        Increment
+			<button onClick={() => handleClick('increment')} className="btn btn-success">
+				Increment
       </button>
-    </>
-  );
+		</>
+	);
 };
 
 export default Counter;
